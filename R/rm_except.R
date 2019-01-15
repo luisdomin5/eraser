52963a785b1d8fc7fab5607793f0e2decfec324f
suzan baert <suzan.baert@outlook.com>
2019-01-15 21:49:35 +0100

    rm_except add test argument

--- R/rm_except.R
+++ R/rm_except.R
@@ -5,7 +5,10 @@
 #'The files that should remain in memory can be listed as a vector or as a list.
 #'
 #'
-#' @param files List or vector of filenames that should not be removed. Please note that filenames should be quoted
+#' @param objects List or vector of object names that should not be removed. Please note that filenames should be quoted
+#'
+#' @param remove_action By default, remove_action is TRUE and will remove all files except the ones listed in objects.
+#' If FALSE, the function will return a list of all functions that will be removed.
 #'
 #' @return removes files from global environment
 #' @export
@@ -22,16 +25,22 @@
 #'
 #'
 
-
-rm_except <- function(files) {
+rm_except <- function(objects, remove_action = TRUE) {
 
   in_memory <- ls(name = .GlobalEnv)
-  files_to_remove <- in_memory[!in_memory %in% files]
+  objects_to_remove <- in_memory[!in_memory %in% objects]
 
-  if (length(in_memory) == length(files_to_remove)) {
+  if (length(in_memory) == length(objects_to_remove)) {
     stop("Attempt to erase full history stopped. Please ensure the exception files are correctly spelled and quoted.")
   }
 
-  rm(list=files_to_remove, envir = .GlobalEnv)
+
+  if (!remove_action){
+    return(objects_to_remove)
+  }
+  else {
+    rm(list = objects_to_remove, envir = .GlobalEnv)
+  }
+
 }
 
